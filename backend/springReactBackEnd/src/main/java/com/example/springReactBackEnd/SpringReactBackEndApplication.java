193e9ebc892b5ad80ccc4195dd269a995c3b81d9
shrihari1363@gmail.com <105267301+ShriHari1307@users.noreply.github.com>
2025-01-06 15:28:21 +0530

    pushing all the files into git

---
+++ backend/springReactBackEnd/src/main/java/com/example/springReactBackEnd/SpringReactBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.example.springReactBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactBackEndApplication.class, args);
+		System.out.println("This is the updated file");
+	}
+
+}
---
+++ backend/springReactBackEnd/src/test/java/com/example/springReactBackEnd/SpringReactBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springReactBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringReactBackEndApplication.class)
+class SpringReactBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
